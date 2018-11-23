bf5d9ac487ea264cfe9fc268a05fd11e81be26ad
Oli S <olli.seiffermann@outlook.de>
2018-11-23 22:56:56 +0100

    improved classes by implementing heritage from corresponding parent classes

--- apple.py
+++ apple.py
@@ -1,24 +1,7 @@
-import random
+from gameObject import GameObject
 
 
-class Apple:
-    def __init__(self, x_range, y_range):
-        self.thickness = 20
-        self.x_coordinate = self.get_x(x_range)
-        self.y_coordinate = self.get_y(y_range)
+class Apple(GameObject):
+    def __init__(self, x_range, y_range, thickness):
+        super().__init__(x_range, y_range, thickness)
         self.is_eaten = False
-
-    def get_x(self, x_range):
-        return round(random.randrange(x_range[0], x_range[1] - self.thickness) / self.thickness) * self.thickness
-
-    def get_y(self, y_range):
-        return round(random.randrange(y_range[0], y_range[1] - self.thickness) / self.thickness) * self.thickness
-
-    def __str__(self):
-        return "Apple @" + str(self.x_coordinate) + " x " + str(self.y_coordinate)
-
-    def get_position(self):
-        return [self.x_coordinate, self.y_coordinate, self.thickness, self.thickness]
-
-    def get_thickness(self):
-        return self.thickness
--- snake.py
+++ snake.py
@@ -1,17 +1,9 @@
-import random
+from movableGameObject import MovableGameObject
 
 
-class Snake:
-    def __init__(self, window_width, window_height, block_size):
-        # game info
-        self.block_size = block_size
-        self.window_width = window_width[0]
-        self.window_height = [window_height[0], window_height[1]]
-
-        # snake position
-        # todo spawn at a proper position mod thickness
-        self.x_coordinate = self.window_width // 2
-        self.y_coordinate = self.window_height[1] // 2
+class Snake(MovableGameObject):
+    def __init__(self, x_range, y_range, thickness, block_size):
+        super().__init__(x_range, y_range, thickness, block_size)
         self.direction = "up"
 
         # snake body
@@ -19,27 +11,7 @@ class Snake:
         self.length = 1
         self.body_parts = [[self.x_coordinate, self.y_coordinate]]
 
-    def __str__(self):
-        return "Snake @" + str(self.x_coordinate) + " x " + str(self.y_coordinate)
-
-    def move(self):
-        self.calculate_new_position()
-        self.check_boundaries()
-
-    def check_boundaries(self):
-        # snake goes through right screen border
-        if self.x_coordinate >= self.window_width:
-            self.x_coordinate = 0
-        # snake goes through left screen border
-        if self.x_coordinate < 0:
-            self.x_coordinate = self.window_width
-        # snake goes through bottom border
-        if self.y_coordinate >= self.window_height[1]:
-            self.y_coordinate = self.window_height[0]
-        # snake goes through top border
-        if self.y_coordinate < self.window_height[0]:
-            self.y_coordinate = self.window_height[1] - self.thickness
-
+    # ########  override  ######## #
     def calculate_new_position(self):
         if self.direction == "left":
             self.move_left()
@@ -50,30 +22,4 @@ class Snake:
         if self.direction == "down":
             self.move_down()
 
-    def move_left(self):
-        self.x_coordinate -= self.block_size
 
-    def move_right(self):
-        self.x_coordinate += self.block_size
-
-    def move_up(self):
-        self.y_coordinate -= self.block_size
-
-    def move_down(self):
-        self.y_coordinate += self.block_size
-
-    def collides_with(self, other):
-        if other.get_position()[0] + other.get_thickness() > self.get_position()[0] >= other.get_position()[0] or  \
-           other.get_position()[0] < self.get_position()[0] + self.get_thickness() < other.get_position()[0] + other.get_thickness():
-            if other.get_position()[1] + other.get_thickness() > self.get_position()[1] > other.get_position()[1]:
-                return True
-            elif other.get_position()[1] + other.get_thickness() >= self.get_position()[1] + self.get_thickness() > other.get_position()[1]:
-                return True
-        else:
-            return False
-
-    def get_position(self):
-        return self.x_coordinate, self.y_coordinate
-
-    def get_thickness(self):
-        return self.thickness
