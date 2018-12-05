b3c2fe651eff9f5bac9a8d1ffeb8d433f6e310ac
Oli S <olli.seiffermann@outlook.de>
2018-12-05 17:11:09 +0100

    improved collision detection and border movement. stretched the screen a little bit further

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 from apple import Apple
 from snake import Snake
 
@@ -14,10 +16,10 @@ class Game:
     LIGHT_GREY = (211, 211, 211)
 
     def __init__(self):
-        # window related stufff
+        # window related stuff
         self.window_height = 600
-        self.window_width = 800
-        self.playfield_height = self.window_height - 100
+        self.window_width = 900
+        self.playfield_height = self.window_height - 120
         self.playfield_width = self.window_width
         self.window_caption = "Slither.ie"
         self.hud_color = self.BLACK
@@ -26,13 +28,15 @@ class Game:
         self.fps = 10
         self.block_size = 20
         self.player_score = 0
-        self.player_name = "HANSPETER_ÜÄÖ"
+        self.player_name = "Player_1"
         self.game_over = False
         self.apple = None
         self.snake = None
         self.apple_color = self.RED
 
         # initialize pygame stuff
+        pygame.mixer.pre_init(44100, -16, 2, 512)
+        pygame.mixer.init()
         pygame.init()
         pygame.display.set_caption("slither.ie")
         self.game_window = pygame.display.set_mode((self.window_width, self.window_height))
@@ -44,10 +48,14 @@ class Game:
         self.snake_head_sprite = {}
         self.snake_body_sprite = {}
 
+        # "initialize" sounds due to readability
+        self.snake_eating_sounds = []
+
         self.start_game()
 
     def start_game(self):
         self.load_sprites()
+        self.load_sounds()
         self.spawn_player()
         self.spawn_new_apple()
         self.draw_screen()
@@ -55,12 +63,12 @@ class Game:
         while not self.game_over:
             # process events
             self.process_events(pygame.event.get())
-            self.check_collisions()
 
             self.calculate_frame()
 
             # spawn a new apple if the old one got eaten and draw it to the screen
             if self.apple.is_eaten:
+                pygame.mixer.Sound.play(random.choice(self.snake_eating_sounds))
                 self.spawn_new_apple()
 
             # update window
@@ -71,24 +79,10 @@ class Game:
             self.display_player_name()
             pygame.display.update()
             self.game_clock.tick(self.fps)
+            self.check_collisions()
 
         pygame.quit()
         quit(0)
-
-    def draw_screen(self):
-        self.game_window.fill(self.hud_color)
-        pygame.draw.rect(self.game_window, self.WHITE, [0, 50, self.playfield_width, self.playfield_height])
-
-    def display_player_score(self):
-        score_string = "Score: " + str(self.player_score)
-        text_surface = self.text_font.render(score_string, True, self.WHITE)
-        # text_surface.get_rect().center = (self.window_width / 2), 10
-        self.game_window.blit(text_surface, text_surface.get_rect())
-
-    def display_player_name(self):
-        name_string = "Name: " + str(self.player_name)
-        text_surface = self.text_font.render(name_string, True, self.WHITE)
-        self.game_window.blit(text_surface, (self.window_width - text_surface.get_width(), 0))
 
     def load_sprites(self):
         # initialize apple sprite
@@ -118,6 +112,20 @@ class Game:
         self.snake_body_sprite["right-up"] = pygame.transform.rotate(self.snake_body_sprite["right-down"], 270)
         self.snake_body_sprite["down-left"] = self.snake_body_sprite["right-up"]
 
+    def load_sounds(self):
+        self.snake_eating_sounds.append(pygame.mixer.Sound("sounds/snake_eating_v1.wav"))
+        self.snake_eating_sounds.append(pygame.mixer.Sound("sounds/snake_eating_v2.wav"))
+
+    def spawn_player(self):
+        self.snake = Snake([0, self.playfield_width], [60, self.playfield_height + 60], self.block_size, self.block_size)
+
+    def spawn_new_apple(self):
+        self.apple = Apple([0, self.playfield_width], [60, self.playfield_height + 60], self.block_size)
+
+    def draw_screen(self):
+        self.game_window.fill(self.hud_color)
+        pygame.draw.rect(self.game_window, self.WHITE, [0, 60, self.playfield_width, self.playfield_height])
+
     def process_events(self, events):
         for event in events:
             if event.type == pygame.KEYDOWN:
@@ -138,14 +146,22 @@ class Game:
                 elif event.key == pygame.K_DOWN:
                     self.snake.head.set_direction("down")
 
-    def spawn_new_apple(self):
-        self.apple = Apple([0, self.playfield_width], [50, self.playfield_height + 50], self.block_size)
+    def check_collisions(self):
+        if self.snake.collides_with(self.apple):
+            self.snake.length += 1
+            self.apple.is_eaten = True
+            self.player_score += 1
+            print("{} ate {}".format(str(self.snake), str(self.apple)))
+
+        for body_part in self.snake.get_body_parts()[1:]:
+            if self.snake.head.get_position() == body_part.get_position():
+                self.game_over = True
+
+    def calculate_frame(self):
+        self.snake.move()
 
     def draw_apple(self):
         self.game_window.blit(self.apple_sprite, self.apple.get_position_thickness_thickness())
-
-    def spawn_player(self):
-        self.snake = Snake([0, self.playfield_width], [50, self.playfield_height + 50], self.block_size, self.block_size)
 
     def draw_player(self):
         if self.snake.head.get_direction() == "up":
@@ -193,19 +209,16 @@ class Game:
             except IndexError:
                 pass
 
-    def check_collisions(self):
-        if self.snake.collides_with(self.apple):
-            self.snake.length += 1
-            self.apple.is_eaten = True
-            self.player_score += 1
-            print("{} ate {}".format(str(self.snake), str(self.apple)))
+    def display_player_score(self):
+        score_string = "Score: " + str(self.player_score)
+        text_surface = self.text_font.render(score_string, True, self.WHITE)
+        # text_surface.get_rect().center = (self.window_width / 2), 10
+        self.game_window.blit(text_surface, (10, 20))
 
-        for body_part in self.snake.get_body_parts()[:-1]:
-            if self.snake.get_body_parts()[-1].get_position() == body_part.get_position():
-                self.game_over = True
-
-    def calculate_frame(self):
-        self.snake.move()
+    def display_player_name(self):
+        name_string = "Name: " + str(self.player_name)
+        text_surface = self.text_font.render(name_string, True, self.WHITE)
+        self.game_window.blit(text_surface, (self.window_width - (text_surface.get_width() + 10), 20))
 
 
 this = Game()
